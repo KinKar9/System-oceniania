f2ecda39a5548cf4125c878b2d1fd65a0c13a722
St00hu <sajewicz.konrad@op.pl>
2026-05-30 18:49:33 +0200

    Faza 2

---
+++ system-oceniania/src/main/java/pl/studenci/systemoceniania/service/StudentService.java
@@ -0,0 +1,33 @@
+package pl.studenci.systemoceniania.service;
+
+import org.springframework.stereotype.Service;
+import pl.studenci.systemoceniania.entity.Student;
+import pl.studenci.systemoceniania.repository.StudentRepository;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository repository;
+
+    public StudentService(StudentRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Student> findAll() {
+        return repository.findAll();
+    }
+
+    public Student findById(Long id) {
+        return repository.findById(id).orElseThrow();
+    }
+
+    public Student save(Student student) {
+        return repository.save(student);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+}
